6705d8f9ecbe971ec178a00a4cf3264a42f1a2a0
Lokesh <lokesh.gahlot@wellsfargo.com>
2016-01-28 09:01:46 -0800

    Route logic edd in app.js On branch setup-routes Changes to be committed: modified:   app/js/app.js

--- app/js/app.js
+++ app/js/app.js
@@ -4,10 +4,30 @@ window.ContactManager = {
   Views:{},
   start: function(data) {
     var contacts = new ContactManager.Collections.Contacts(data.contacts);
-    var contactsView = new ContactManager.Views.Contacts({
-       collection: contacts
-     });
+    var router = new ContactManager.Router();
 
-    $('.main-container').html(contactsView.render().$el);
+    router.on('route:home', function() {
+      router.navigate('contacts', {
+        trigger: true,
+        replace: true
+      });
+    });
+
+    router.on('route:showContacts', function() {
+      var contactsView = new ContactManager.Views.Contacts({
+        collection: contacts
+      });
+      $('.main-container').html(contactsView.render().$el);
+    });
+
+    router.on('route:newContacts', function() {
+      console.log('new Contact');
+    });
+
+    router.on('route:editContacts', function(id) {
+      console.log('Edit Contacts id = ', id);
+    });
+
+    Backbone.history.start();
   }
 };
