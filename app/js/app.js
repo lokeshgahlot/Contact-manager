30d00fc41acd12592341cc589baee4058e1234fd
Lokesh <lokesh.gahlot@wellsfargo.com>
2016-01-28 08:39:59 -0800

    Project start here ...app.js added On branch render-all Changes to be committed: new file:   app/js/app.js

---
+++ app/js/app.js
@@ -0,0 +1,13 @@
+window.ContactManager = {
+  Models:{},
+  Collections:{},
+  Views:{},
+  start: function(data) {
+    var contacts = new ContactManager.Collections.Contacts(data.contacts);
+    var contactsView = new ContactManager.Views.Contacts({
+       collection: contacts
+     });
+
+    $('.main-container').html(contactsView.render().$el);
+  }
+};
