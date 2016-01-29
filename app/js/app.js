62c7b4c73912edefe004140cce64118429b48e01
Lokesh <lokesh.gahlot@wellsfargo.com>
2016-01-29 09:44:03 -0800

    Edit contact route logic added. On branch edit-contact-module Changes to be committed: modified:   app/js/app.js

--- app/js/app.js
+++ app/js/app.js
@@ -21,7 +21,9 @@ window.ContactManager = {
     });
 
     router.on('route:newContacts', function() {
-      var newContactForm = new ContactManager.Views.ContactForm();
+      var newContactForm = new ContactManager.Views.ContactForm({
+        model: new ContactManager.Models.Contact()
+      });
 
       newContactForm.on('form:submitted', function(attrs) {
         attrs.id = contacts.isEmpty() ? 1 : (_.max(contacts.pluck('id')) + 1);
@@ -33,7 +35,21 @@ window.ContactManager = {
     });
 
     router.on('route:editContacts', function(id) {
-      console.log('Edit Contacts id = ', id);
+      var contact = contacts.get(id);
+      var editContact;
+      if (contact) {
+        editContact  = new ContactManager.Views.ContactForm({
+          model: contact
+        });
+        editContact.on('form:submitted', function(attrs) {
+          contact.set(attrs);
+          router.navigate('contacts', true);
+        });
+
+        $('.main-container').html(editContact.render().$el);
+      } else {
+        router.navigate('contacts', true) ;
+      }
     });
 
     Backbone.history.start();
